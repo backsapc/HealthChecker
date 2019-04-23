1e8d400f5c7d1fa61697a48d0464fdb0cb83c08d
backsapce <backsapce@hotmail.com>
2019-04-23 12:24:52 +0300

    Added CheckViewModel.

--- src/main/scala/backsapc/healthchecker/checker/contracts/CheckerService.scala
+++ src/main/scala/backsapc/healthchecker/checker/contracts/CheckerService.scala
@@ -2,21 +2,21 @@ package backsapc.healthchecker.checker.contracts
 import java.util.UUID
 
 import backsapc.healthchecker.checker._
-import backsapc.healthchecker.checker.domain.Check
+import backsapc.healthchecker.checker.domain.CheckViewModel
 
 import scala.concurrent.Future
 
 trait CheckerService {
-  def getAll(): Future[Seq[Check]]
-  def getById(id: UUID, userId: UUID): Future[Option[Check]]
-  def getAllForUserId(userId: UUID): Future[Seq[Check]]
+  def getAll(): Future[Seq[CheckViewModel]]
+  def getById(id: UUID, userId: UUID): Future[Option[CheckViewModel]]
+  def getAllForUserId(userId: UUID): Future[Seq[CheckViewModel]]
   def delete(id: UUID, userId: UUID): Future[Unit]
   def pause(id: UUID, userId: UUID): Future[Unit]
   def activate(id: UUID, userId: UUID): Future[Unit]
-  def createHttpCheck(createHttpCheck: CreateHttpCheck, userId: UUID): Future[Check]
-  def updateHttpCheck( updateHttpCheck: UpdateHttpCheck, userId: UUID): Future[Check]
-  def createContentCheck(createContentCheck: CreateContentCheck, userId: UUID): Future[Check]
-  def updateContentCheck(updateContentCheck: UpdateContentCheck, userId: UUID): Future[Check]
-  def createPingCheck(createPingCheck: CreatePingCheck, userId: UUID): Future[Check]
-  def updatePingCheck(updatePingCheck: UpdatePingCheck, userId: UUID): Future[Check]
+  def createHttpCheck(createHttpCheck: CreateHttpCheck, userId: UUID): Future[CheckViewModel]
+  def updateHttpCheck(updateHttpCheck: UpdateHttpCheck, userId: UUID): Future[CheckViewModel]
+  def createContentCheck(createContentCheck: CreateContentCheck, userId: UUID): Future[CheckViewModel]
+  def updateContentCheck(updateContentCheck: UpdateContentCheck, userId: UUID): Future[CheckViewModel]
+  def createPingCheck(createPingCheck: CreatePingCheck, userId: UUID): Future[CheckViewModel]
+  def updatePingCheck(updatePingCheck: UpdatePingCheck, userId: UUID): Future[CheckViewModel]
 }
--- src/main/scala/backsapc/healthchecker/checker/domain/Checker.scala
+++ src/main/scala/backsapc/healthchecker/checker/domain/Checker.scala
@@ -1,22 +1,83 @@
 package backsapc.healthchecker.checker.domain
+import java.time.OffsetDateTime
 import java.util.UUID
 
 case class Check(
-  id: UUID,
-  userId: UUID,
-  friendlyName: String,
-  interval: Int,
-  isDeleted: Boolean,
-  isPaused: Boolean,
-  checkType: CheckType.Value,
-  url: Option[String],
-  content: Option[String],
-  ip: Option[String],
-  port: Option[Int]
+    id: UUID,
+    userId: UUID,
+    friendlyName: String,
+    interval: Int,
+    isDeleted: Boolean,
+    isPaused: Boolean,
+    checkType: CheckType.Value,
+    url: Option[String],
+    content: Option[String],
+    ip: Option[String],
+    port: Option[Int],
+    lastCheck: OffsetDateTime,
+    inProgress: Boolean
+)
+
+case class CheckViewModel(
+    id: UUID,
+    userId: UUID,
+    friendlyName: String,
+    interval: Int,
+    isPaused: Boolean,
+    checkType: CheckType.Value,
+    url: Option[String],
+    content: Option[String],
+    ip: Option[String],
+    port: Option[Int]
+)
+
+case class CheckEvent(
+    id: UUID,
+    checkId: UUID,
+    createdAt: OffsetDateTime
 )
 
 object CheckType extends Enumeration {
-  val Http = Value("Http")
+  val Http            = Value("Http")
   val HttpWithContent = Value("Content")
-  val Ping = Value("Ping")
+  val Ping            = Value("Ping")
+}
+
+case class HttpCheckModel(
+    id: UUID,
+    userId: UUID,
+    url: String
+)
+
+case class HttpContentCheckModel(
+    id: UUID,
+    userId: UUID,
+    url: String,
+    content: String
+)
+
+case class PingCheckModel(
+    id: UUID,
+    userId: UUID,
+    ip: String,
+    port: Int
+)
+
+object Mappings {
+  import scala.language.implicitConversions
+  implicit def checkToCheckModel(check: Check): CheckViewModel = {
+    println(check)
+    CheckViewModel(
+      id = check.id,
+      userId = check.userId,
+      friendlyName = check.friendlyName,
+      interval = check.interval,
+      isPaused = check.isPaused,
+      checkType = check.checkType,
+      url = check.url,
+      content = check.content,
+      ip = check.ip,
+      port = check.port
+    )
+  }
 }
