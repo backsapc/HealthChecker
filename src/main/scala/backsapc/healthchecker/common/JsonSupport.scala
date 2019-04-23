ee9e3d2a30aba325ac20b554c2b752515f7327ed
backsapce <backsapce@hotmail.com>
2019-04-23 12:28:21 +0300

    Moved EnumJsonConvert from CheckJsonSupport to JsonSupport.

--- src/main/scala/backsapc/healthchecker/common/JsonSupport.scala
+++ src/main/scala/backsapc/healthchecker/common/JsonSupport.scala
@@ -1,11 +1,12 @@
 package backsapc.healthchecker.common
 
 //#json-support
-import java.util.UUID
+import java.text.{DateFormat, SimpleDateFormat}
+import java.util.{Date, UUID}
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import backsapc.healthchecker.user.bcrypt.BcryptHash
-import spray.json.{DeserializationException, JsString, JsValue, JsonFormat}
+import spray.json.{DeserializationException, JsString, JsValue, JsonFormat, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport {
   // import the default encoders for primitive types (Int, String, Lists etc)
@@ -13,13 +14,12 @@ trait JsonSupport extends SprayJsonSupport {
   implicit object UUIDFormat extends JsonFormat[UUID] {
     def write(uuid: UUID) = JsString(uuid.toString)
 
-    def read(value: JsValue): UUID = {
+    def read(value: JsValue): UUID =
       value match {
         case JsString(uuid) => UUID.fromString(uuid)
         case _ =>
           throw DeserializationException("Expected hexadecimal UUID string")
       }
-    }
   }
 
   implicit object BrcyptHashFormat extends JsonFormat[BcryptHash] {
@@ -30,4 +30,27 @@ trait JsonSupport extends SprayJsonSupport {
       case _           => throw DeserializationException("String expected")
     }
   }
+
+  implicit object JavaDateFormat extends JsonFormat[Date] {
+    def write(m: Date) = JsString(s"${m.toString}")
+
+    def read(json: JsValue): Date = json match {
+      case JsString(s) => new SimpleDateFormat().parse(s)
+      case _           => throw DeserializationException("String expected")
+    }
+  }
+
+  class EnumJsonConverter[T <: scala.Enumeration](enu: T) extends RootJsonFormat[T#Value] {
+    override def write(obj: T#Value): JsValue = JsString(obj.toString)
+
+    override def read(json: JsValue): T#Value =
+      json match {
+        case JsString(txt) => enu.withName(txt)
+        case somethingElse =>
+          throw DeserializationException(
+            s"Expected a value from enum $enu instead of $somethingElse"
+          )
+      }
+  }
+
 }
