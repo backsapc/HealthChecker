62ebaf19536962d958bb8556015e60f122f7cb4a
backsapce <backsapce@hotmail.com>
2019-04-23 18:44:21 +0300

    Changed result base type to trait in user module.

--- src/main/scala/backsapc/healthchecker/user/Contracts/UserService.scala
+++ src/main/scala/backsapc/healthchecker/user/Contracts/UserService.scala
@@ -8,16 +8,16 @@ import backsapc.healthchecker.user.RegisterRequest
 import scala.concurrent.Future
 
 object UserServiceOperationResults {
-  sealed class RegisterResult
-  final case class RegisterSuccess(account: AccountViewModel)
-      extends RegisterResult
-  final case class IdConflict(id: UUID) extends RegisterResult
-  final case class LoginConflict(login: String) extends RegisterResult
-  final case class EmailConflict(email: String) extends RegisterResult
+  sealed trait RegisterResult
+  final case class RegisterSuccess(account: AccountViewModel) extends RegisterResult
+  final case class IdConflict(id: UUID)                       extends RegisterResult
+  final case class LoginConflict(login: String)               extends RegisterResult
+  final case class EmailConflict(email: String)               extends RegisterResult
+  final case class ConfirmationError(email: String)           extends RegisterResult
 
-  sealed class UpdateResult
-  case class UpdateSuccess() extends UpdateResult
-  case class NoSuchUserError(id: UUID) extends UpdateResult
+  sealed trait UpdateResult
+  case class UpdateSuccess()                   extends UpdateResult
+  case class NoSuchUserError(id: UUID)         extends UpdateResult
   case class InvalidPassword(password: String) extends UpdateResult
 }
 
@@ -27,9 +27,9 @@ trait UserService {
   def register(account: RegisterRequest): Future[RegisterResult]
 
   def update(
-    id: UUID,
-    oldPassword: String,
-    newPassword: String
+      id: UUID,
+      oldPassword: String,
+      newPassword: String
   ): Future[UpdateResult]
 
   def findById(id: UUID): Future[Option[AccountViewModel]]
--- src/main/scala/backsapc/healthchecker/user/UserJsonSupport.scala
+++ src/main/scala/backsapc/healthchecker/user/UserJsonSupport.scala
@@ -2,29 +2,23 @@ package backsapc.healthchecker.user
 
 //#json-support
 import backsapc.healthchecker.common.JsonSupport
-import backsapc.healthchecker.user.Contracts.TokenServiceOperationResults.{
-  GenerateResult,
-  WrongPasswordError
-}
+import backsapc.healthchecker.user.Contracts.TokenServiceOperationResults.WrongPasswordError
 import backsapc.healthchecker.user.Contracts.UserServiceOperationResults._
-import backsapc.healthchecker.user.Contracts.{
-  AccountViewModel,
-  TokenServiceOperationResults
-}
+import backsapc.healthchecker.user.Contracts.{ AccountViewModel, TokenServiceOperationResults }
 import backsapc.healthchecker.user.domain.Account
 
 trait UserJsonSupport extends JsonSupport {
   // import the default encoders for primitive types (Int, String, Lists etc)
   import spray.json.DefaultJsonProtocol._
 
-  implicit val accountJsonFormat = jsonFormat4(Account)
-  implicit val loginRequestJsonFormat = jsonFormat2(LoginRequest)
-  implicit val registerRequestJsonFormat = jsonFormat4(RegisterRequest)
-  implicit val accountViewModelJsonFormat = jsonFormat3(AccountViewModel)
-  implicit val loginConflictJsonFormat = jsonFormat1(LoginConflict)
-  implicit val idConflictJsonFormat = jsonFormat1(IdConflict)
-  implicit val emailConflictJsonFormat = jsonFormat1(EmailConflict)
-  implicit val registerResult = jsonFormat0(() => new RegisterResult)
+  implicit val accountJsonFormat           = jsonFormat4(Account)
+  implicit val loginRequestJsonFormat      = jsonFormat2(LoginRequest)
+  implicit val registerRequestJsonFormat   = jsonFormat4(RegisterRequest)
+  implicit val accountViewModelJsonFormat  = jsonFormat3(AccountViewModel)
+  implicit val loginConflictJsonFormat     = jsonFormat1(LoginConflict)
+  implicit val idConflictJsonFormat        = jsonFormat1(IdConflict)
+  implicit val emailConflictJsonFormat     = jsonFormat1(EmailConflict)
+  implicit val confirmationErrorJsonFormat = jsonFormat1(ConfirmationError)
 
   implicit val wrongPasswordJsonFormat = jsonFormat1(WrongPasswordError)
   implicit val noSuchUserJsonFormat = jsonFormat1(
@@ -33,10 +27,8 @@ trait UserJsonSupport extends JsonSupport {
   implicit val generateSuccessJsonFormat = jsonFormat1(
     TokenServiceOperationResults.GenerateSuccess
   )
-  implicit val generateResult = jsonFormat0(() => new GenerateResult)
 
   implicit val wrongPasswordUpdateJsonFormat = jsonFormat1(InvalidPassword)
-  implicit val noSuchUserUpdateJsonFormat = jsonFormat1(NoSuchUserError)
-  implicit val updateSuccessJsonFormat = jsonFormat0(UpdateSuccess)
-  implicit val updateResult = jsonFormat0(() => new UpdateResult)
+  implicit val noSuchUserUpdateJsonFormat    = jsonFormat1(NoSuchUserError)
+  implicit val updateSuccessJsonFormat       = jsonFormat0(UpdateSuccess)
 }
