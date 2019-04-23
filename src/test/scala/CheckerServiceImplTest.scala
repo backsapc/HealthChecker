507e3b461025d865b8d3c5290f8a7f971fc9db5c
backsapce <backsapce@hotmail.com>
2019-04-23 12:30:31 +0300

    Updated CheckerServiceImplementation tests.

--- src/test/scala/CheckerServiceImplTest.scala
+++ src/test/scala/CheckerServiceImplTest.scala
@@ -1,8 +1,9 @@
+import java.time.OffsetDateTime
 import java.util.UUID
 
 import backsapc.healthchecker.checker._
 import backsapc.healthchecker.checker.dao.CheckerRepository
-import backsapc.healthchecker.checker.domain.{ Check, CheckType }
+import backsapc.healthchecker.checker.domain.{ Check, CheckType, CheckViewModel }
 import backsapc.healthchecker.checker.implementation.CheckerServiceImpl
 import org.scalamock.scalatest.AsyncMockFactory
 import org.scalatest.{ AsyncFlatSpec, Matchers }
@@ -10,7 +11,7 @@ import org.scalatest.{ AsyncFlatSpec, Matchers }
 import scala.concurrent.Future
 
 class CheckerServiceImplTest extends AsyncFlatSpec with AsyncMockFactory with Matchers {
-
+  import backsapc.healthchecker.checker.domain.Mappings._
   val mockCheckerRepository: CheckerRepository = stub[CheckerRepository]
 
   val checkerService: CheckerServiceImpl =
@@ -38,7 +39,9 @@ class CheckerServiceImplTest extends AsyncFlatSpec with AsyncMockFactory with Ma
     Some(url),
     None,
     None,
-    None
+    None,
+    OffsetDateTime.now(),
+    inProgress = false
   )
 
   val mockCreateHttpCheck: CreateHttpCheck =
@@ -62,32 +65,36 @@ class CheckerServiceImplTest extends AsyncFlatSpec with AsyncMockFactory with Ma
   behavior of "CheckerServiceImpl"
 
   "Checker service " should " create http check" in {
+    val expected: CheckViewModel = mockCheck
     (mockCheckerRepository.save _).when(*).returns(Future successful mockCheck)
     (mockCheckerRepository.existsWithId _).when(*).returns(Future successful false)
-    checkerService.createHttpCheck(mockCreateHttpCheck, userId).map(_ shouldBe mockCheck)
+    checkerService.createHttpCheck(mockCreateHttpCheck, userId).map(_ shouldBe expected)
   }
 
   "Checker service " should " update http check" in {
+    val expected: CheckViewModel = mockCheck.copy(url = Some(anotherUrl))
     (mockCheckerRepository.get _).when(*, *).returns(Future successful Some(mockCheck))
     (mockCheckerRepository.update _)
       .when(*, *)
       .returns(Future successful mockCheck.copy(url = Some(anotherUrl)))
     checkerService
       .updateHttpCheck(mockUpdateHttpCheck.copy(url = anotherUrl), userId)
-      .map(_ shouldBe mockCheck.copy(url = Some(anotherUrl)))
+      .map(_ shouldBe expected)
   }
 
   "Checker service " should " create content check" in {
+    val expected: CheckViewModel = mockCheck.copy(content = Some(content))
     (mockCheckerRepository.save _)
       .when(*)
       .returns(Future successful mockCheck.copy(content = Some(content)))
     (mockCheckerRepository.existsWithId _).when(*).returns(Future successful false)
     checkerService
       .createContentCheck(mockCreateContentCheck, userId)
-      .map(_ shouldBe mockCheck.copy(content = Some(content)))
+      .map(_ shouldBe expected)
   }
 
   "Checker service " should " update content check" in {
+    val expected: CheckViewModel = mockCheck.copy(url = Some(anotherUrl), content = Some(content))
     (mockCheckerRepository.get _)
       .when(*, *)
       .returns(Future successful Some(mockCheck.copy(content = Some(content))))
@@ -96,20 +103,22 @@ class CheckerServiceImplTest extends AsyncFlatSpec with AsyncMockFactory with Ma
       .returns(Future successful mockCheck.copy(url = Some(anotherUrl), content = Some(content)))
     checkerService
       .updateContentCheck(mockUpdateContentCheck.copy(url = anotherUrl), userId)
-      .map(_ shouldBe mockCheck.copy(url = Some(anotherUrl), content = Some(content)))
+      .map(_ shouldBe expected)
   }
 
   "Checker service " should " create ping check" in {
+    val expected: CheckViewModel = mockCheck.copy(ip = Some(ip), port = Some(port))
     (mockCheckerRepository.save _)
       .when(*)
       .returns(Future successful mockCheck.copy(ip = Some(ip), port = Some(port)))
     (mockCheckerRepository.existsWithId _).when(*).returns(Future successful false)
     checkerService
       .createPingCheck(mockCreatePingCheck, userId)
-      .map(_ shouldBe mockCheck.copy(ip = Some(ip), port = Some(port)))
+      .map(_ shouldBe expected)
   }
 
   "Checker service " should " update ping check" in {
+    val expected: CheckViewModel = mockCheck.copy(ip = Some(anotherIp), port = Some(port))
     (mockCheckerRepository.get _)
       .when(*, *)
       .returns(Future successful Some(mockCheck.copy(ip = Some(ip), port = Some(port))))
@@ -118,6 +127,6 @@ class CheckerServiceImplTest extends AsyncFlatSpec with AsyncMockFactory with Ma
       .returns(Future successful mockCheck.copy(ip = Some(anotherIp), port = Some(port)))
     checkerService
       .updatePingCheck(mockUpdatePingCheck.copy(ip = anotherIp), userId)
-      .map(_ shouldBe mockCheck.copy(ip = Some(anotherIp), port = Some(port)))
+      .map(_ shouldBe expected)
   }
 }
