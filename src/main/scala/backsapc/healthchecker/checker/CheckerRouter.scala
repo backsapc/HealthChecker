2535c57b06fa7d4b0a87af8bc55093149e99e31e
backsapce <backsapce@hotmail.com>
2019-04-23 12:23:52 +0300

    Fixed bug with checker router.

--- src/main/scala/backsapc/healthchecker/checker/CheckerRouter.scala
+++ src/main/scala/backsapc/healthchecker/checker/CheckerRouter.scala
@@ -12,119 +12,119 @@ import backsapc.healthchecker.common.JwtService
 import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 case class CreateHttpCheck(id: UUID, interval: Int, friendlyName: String, url: String)
 case class UpdateHttpCheck(id: UUID, interval: Int, friendlyName: String, url: String)
 case class CreateContentCheck(
-  id: UUID,
-  friendlyName: String,
-  interval: Int,
-  url: String,
-  content: String
+    id: UUID,
+    friendlyName: String,
+    interval: Int,
+    url: String,
+    content: String
 )
 case class UpdateContentCheck(
-  id: UUID,
-  friendlyName: String,
-  interval: Int,
-  url: String,
-  content: String
+    id: UUID,
+    friendlyName: String,
+    interval: Int,
+    url: String,
+    content: String
 )
 case class CreatePingCheck(id: UUID, interval: Int, friendlyName: String, ip: String, port: Int)
 case class UpdatePingCheck(id: UUID, interval: Int, friendlyName: String, ip: String, port: Int)
 
 class CheckerRouter(checkerService: CheckerService)(
-  implicit executionContext: ExecutionContext
+    implicit executionContext: ExecutionContext
 ) extends JwtService
     with CheckerJsonSupport {
 
-  val routes: Route = (path("check") & authenticated) { claims =>
-    path("http") {
-      post {
-        entity(as[CreateHttpCheck]) { createHttpCheck =>
-          complete {
-            checkerService
-              .createHttpCheck(createHttpCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
-        }
-      } ~ put {
-        entity(as[UpdateHttpCheck]) { updateHttpCheck =>
-          complete {
-            checkerService
-              .updateHttpCheck(updateHttpCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
+  val routes: Route = (path("check" / "http") & authenticated) { claims =>
+    post {
+      entity(as[CreateHttpCheck]) { createHttpCheck =>
+        complete {
+          checkerService
+            .createHttpCheck(createHttpCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
         }
       }
-    } ~ path("content") {
-      post {
-        entity(as[CreateContentCheck]) { createContentCheck =>
-          complete {
-            checkerService
-              .createContentCheck(createContentCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
-        }
-      } ~ put {
-        entity(as[UpdateContentCheck]) { updateContentCheck =>
-          complete {
-            checkerService
-              .updateContentCheck(updateContentCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
+    } ~ put {
+      entity(as[UpdateHttpCheck]) { updateHttpCheck =>
+        complete {
+          checkerService
+            .updateHttpCheck(updateHttpCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
         }
       }
-    } ~ path("ping") {
-      post {
-        entity(as[CreatePingCheck]) { createPingCheck =>
-          complete {
-            checkerService
-              .createPingCheck(createPingCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
-        }
-      } ~ put {
-        entity(as[UpdatePingCheck]) { updatePingCheck =>
-          complete {
-            checkerService
-              .updatePingCheck(updatePingCheck, getId(claims))
-              .map[ToResponseMarshallable](
-                result => StatusCodes.Created -> result
-              )
-          }
+    }
+  } ~ (path("check" / "content") & authenticated) { claims =>
+    post {
+      entity(as[CreateContentCheck]) { createContentCheck =>
+        complete {
+          checkerService
+            .createContentCheck(createContentCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
         }
       }
-    } ~ (get & pathEndOrSingleSlash) {
+    } ~ put {
+      entity(as[UpdateContentCheck]) { updateContentCheck =>
+        complete {
+          checkerService
+            .updateContentCheck(updateContentCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
+        }
+      }
+    }
+  } ~ (path("check" / "ping") & authenticated) { claims =>
+    post {
+      entity(as[CreatePingCheck]) { createPingCheck =>
+        complete {
+          checkerService
+            .createPingCheck(createPingCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
+        }
+      }
+    } ~ put {
+      entity(as[UpdatePingCheck]) { updatePingCheck =>
+        complete {
+          checkerService
+            .updatePingCheck(updatePingCheck, getId(claims))
+            .map[ToResponseMarshallable](
+              result => StatusCodes.Created -> result
+            )
+        }
+      }
+    }
+  } ~ (path("check") & authenticated) { claims =>
+    (get & pathEndOrSingleSlash) {
       onComplete(checkerService.getAllForUserId(getId(claims))) {
         case Success(value) => complete(value)
         case Failure(_)     => complete(StatusCodes.InternalServerError)
       }
-    } ~ path(JavaUUID) { id =>
-      get {
-        complete {
-          checkerService
-            .getById(id, getId(claims))
-            .map[ToResponseMarshallable] {
-              case Some(check) => StatusCodes.OK -> check
-              case None        => StatusCodes.NotFound
-            }
-        }
-      } ~ delete {
-        onComplete(checkerService.delete(id, getId(claims))) {
-          case Success(_) => complete(StatusCodes.NoContent)
-          case Failure(_) => complete(StatusCodes.NotFound)
-        }
+    }
+  } ~ (path("check" / JavaUUID) & authenticated) { (id, claims) =>
+    get {
+      complete {
+        checkerService
+          .getById(id, getId(claims))
+          .map[ToResponseMarshallable] {
+            case Some(check) => StatusCodes.OK -> check
+            case None        => StatusCodes.NotFound
+          }
+      }
+    } ~ delete {
+      onComplete(checkerService.delete(id, getId(claims))) {
+        case Success(_) => complete(StatusCodes.NoContent)
+        case Failure(_) => complete(StatusCodes.NotFound)
       }
     }
   }
